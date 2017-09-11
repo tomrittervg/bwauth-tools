f3c8a8994581dcf05035690265831a4783ad2c74
Tom Ritter <tom@ritter.vg>
2017-09-11 16:08:54 -0500

    Add more bwauths

--- download_files.py
+++ download_files.py
@@ -1,18 +1,38 @@
 #!/usr/bin/env python
 
 import os
+import sys
+import json
 import pytz
-import urllib
+import requests
 from datetime import datetime, timedelta
 
+
 bwauths = {
+	'moria1' : {
+		'type' : 'hourly',
+		'dl_location' : 'data/moria1',
+		'url' : 'https://www.freehaven.net/~arma/bwscan.V3BandwidthsFile',
+		'file_minute' : '10',
+		'tz' : 'UTC',
+		},
 	'maatuska' : {
-		'url' : 'https://bwauth.ritter.vg/bwauth/',
+		'type' : 'archived',
 		'dl_location' : 'data/maatuska',
+		'url' : 'https://bwauth.ritter.vg/bwauth/',
 		'file_minute' : '45',
 		'tz' : 'Canada/Central',
 		'give_up_after' : 10
-		}
+		},
+	'longclaw' : {
+		'type' : 'vote',
+		'dl_location' : 'data/longclaw',
+		'url' : 'https://collector.torproject.org/recent/relay-descriptors/votes/',
+		'fingerprint' : 'EFCBE720AB3A82B99F9E953CD5BF50F7EEFC7B97',
+		'file_minute' : '00',
+		'tz' : 'UTC',
+		'give_up_after' : 5
+		},
 }
 
 def find_files(location):
@@ -28,6 +48,8 @@ def make_dir_structure(base, dt):
 	month = str(dt.month)
 	month_d = os.path.join(year_d, month)
 
+	if not os.path.isdir(base):
+		os.mkdir(base)
 	if not os.path.isdir(year_d):
 		os.mkdir(year_d)
 	if not os.path.exists(month_d):
@@ -39,25 +61,31 @@ def current_filetime(bwauth):
 	return datetime(dt.year, dt.month, dt.day, dt.hour, int(bwauth['file_minute']), 0, 0, tz)
 
 def filetime_to_filename(bwauth, dt):
-	return dt.strftime('bwscan.%Y%m%d-%H') + bwauth['file_minute']	
+	if bwauth['type'] == 'archived':
+		return dt.strftime('bwscan.%Y%m%d-%H') + bwauth['file_minute']
+	elif bwauth['type'] == 'vote':
+		return dt.strftime('%Y-%m-%d-%H-') + bwauth['file_minute'] + '-00-vote-' + bwauth['fingerprint']
+	elif bwauth['type'] == 'hourly':
+		return dt.strftime('%Y-%m-%d-%H-') + bwauth['file_minute'] + '-00'
+	else:
+		raise Exception("Do not know how to format filetime for " + bwauth['dl_location'])
 
-def wget(bwauth, filetime):
-	filename = filetime_to_filename(bwauth, filetime)
-	url = bwauth['url'] + filename
-	destination = os.path.join(bwauth['dl_location'], str(filetime.year), str(filetime.month), filename)
-	
-	make_dir_structure(bwauth['dl_location'], filetime)
-	f = urllib.URLopener()
+def wget(url, destination):
 	try:
-		filename, headers = f.retrieve(url, destination)
+		r = requests.get(url, stream=True)
+		if r.status_code == 404:
+			return False
+		h = open(destination, "wb")
+		for chunk in r.iter_content(chunk_size=512):
+			if chunk:  # filter out keep-alive new chunks
+				h.write(chunk)
 		print "Downloading", url, "to", destination, ": Success"
 		return True
-	except: 
-		print "Downloading", url, "to", destination, ": 404"
+	except Exception as e: 
+		print e
 		return False
 
-
-def download(name, bwauth):
+def download_archived(name, bwauth):
 	filenames = find_files(bwauth['dl_location'])
 	current_time = current_filetime(bwauth)
 	current_name = filetime_to_filename(bwauth, current_time)
@@ -67,17 +95,81 @@ def download(name, bwauth):
 	while num_missing_in_a_row < bwauth['give_up_after']:
 		this_name = filetime_to_filename(bwauth, this_time)
 		if this_name not in filenames:
-			if wget(bwauth, this_time):
+			url = bwauth['url'] + this_name
+			destination = os.path.join(bwauth['dl_location'], str(this_time.year), str(this_time.month), this_name)
+			make_dir_structure(bwauth['dl_location'], this_time)
+
+			if wget(url, destination):
 				num_missing_in_a_row = 0
 			else:
 				num_missing_in_a_row += 1
 		else:
-			print "Skipping", this_name
+			pass
+			#print "Skipping", this_name
 		this_time -= timedelta(hours=1)
 
+def download_hourly(name, bwauth):
+	filenames = find_files(bwauth['dl_location'])
+	current_time = current_filetime(bwauth)
+	current_name = filetime_to_filename(bwauth, current_time)
+
+	url = bwauth['url']
+	destination = os.path.join(bwauth['dl_location'], str(current_time.year), str(current_time.month), current_name)
+	make_dir_structure(bwauth['dl_location'], current_time)
+
+	wget(url, destination)
+
+def download_vote(name, bwauth):
+	def get_url(name_base):
+		url = None
+		for i in jindex['directories']:
+			if i['path'] == 'recent':
+				for j in i['directories']:
+					if j['path'] == 'relay-descriptors':
+						for k in j['directories']:
+							if k['path'] == 'votes':
+								for l in k['files']:
+									if name_base in l['path']:
+										url = bwauth['url'] + l['path']
+		return url
+
+	filenames = find_files(bwauth['dl_location'])
+	current_time = current_filetime(bwauth)
+	current_name = filetime_to_filename(bwauth, current_time)
+
+	r = requests.get('https://collector.torproject.org/index/index.json')
+	jindex = json.loads(r.text)
+
+	this_time = current_time
+	num_missing_in_a_row = 0
+	while num_missing_in_a_row < bwauth['give_up_after']:
+		this_name = filetime_to_filename(bwauth, this_time)
+		if this_name not in filenames:
+			url = get_url(this_name)
+			destination = os.path.join(bwauth['dl_location'], str(this_time.year), str(this_time.month), this_name)
+			make_dir_structure(bwauth['dl_location'], this_time)
+			if url and wget(url, destination):
+				num_missing_in_a_row = 0
+			else:
+				num_missing_in_a_row += 1
+		else:
+			pass
+			#print "Skipping", this_name
+		this_time -= timedelta(hours=1)
+
+def download(name, bwauth):
+	if bwauth['type'] == 'archived':
+		download_archived(name, bwauth)
+	elif bwauth['type'] == 'vote':
+		download_vote(name, bwauth)
+	elif bwauth['type'] == 'hourly':
+		download_hourly(name, bwauth)
+	else:
+		raise Exception("Do not know how to download files from " + name)
 
 def download_all():
 	for k in bwauths:
+		print "Downloading", k
 		download(k, bwauths[k])
 
 
